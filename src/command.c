591da1d6cc34f3f5b5148b2da19e804fea09f311
Alexandre BRAULT <abrault@e2r7p7.42.fr>
2014-01-15 11:59:46 +0100

    Correction Norme

--- src/command.c
+++ src/command.c
@@ -6,7 +6,7 @@
 /*   By: abrault <abrault@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/05 18:32:35 by abrault           #+#    #+#             */
-/*   Updated: 2014/01/13 17:05:55 by abrault          ###   ########.fr       */
+/*   Updated: 2014/01/15 11:59:17 by abrault          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int		on_key_up(t_env *e)
 {
-	if (e->data->map[(int)((e->data->pos_y + sin(ft_rad(e->data->rot)) * VITESSE)
-		/ SIZE_CASE)][(int)((e->data->pos_x + cos(ft_rad(e->data->rot)) * VITESSE)
+	if (e->data->map[(int)((e->data->pos_y +
+						sin(ft_rad(e->data->rot)) * VITESSE) / SIZE_CASE)]
+					[(int)((e->data->pos_x +
+						cos(ft_rad(e->data->rot)) * VITESSE)
 		/ SIZE_CASE)] == 0)
 	{
 		e->data->pos_y += sin(ft_rad(e->data->rot)) * VITESSE;
@@ -26,9 +28,10 @@ int		on_key_up(t_env *e)
 
 int		on_key_down(t_env *e)
 {
-	if (e->data->map[(int)((e->data->pos_y - sin(ft_rad(e->data->rot)) * VITESSE)
-		/ SIZE_CASE)][(int)((e->data->pos_x - cos(ft_rad(e->data->rot)) * VITESSE)
-		/ SIZE_CASE)] == 0)
+	if (e->data->map[(int)((e->data->pos_y -
+						sin(ft_rad(e->data->rot)) * VITESSE) / SIZE_CASE)]
+					[(int)((e->data->pos_x -
+						cos(ft_rad(e->data->rot)) * VITESSE) / SIZE_CASE)] == 0)
 	{
 		e->data->pos_y -= sin(ft_rad(e->data->rot)) * VITESSE;
 		e->data->pos_x -= cos(ft_rad(e->data->rot)) * VITESSE;
